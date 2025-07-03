2fe54a55b309ed7fb7642f9a0f18e31295b71883
abhi-v97 <v.abhishek@outlook.com>
2025-07-03 20:25:25 +0100

    textures are correctly aligned on the wall now, however they are not static

--- src/render/tex_colour.c
+++ src/render/tex_colour.c
@@ -12,25 +12,41 @@
 
 #include "cub3d.h"
 
-// // side: 0 if NS, 1 if WE
-// int	get_colour(t_gdata *data, int side)
-// {
-// 	(void) data;
-// 	(void) side;
-	
-// 	double wall_x;
-// 	if (side == 0)
-// 		wall_x = data->player.pos.y + perp_dist * ray_dir_y;
-// 	else
-// 		wall_x = data->player.pos.x + perp_dist * ray_dir_x;
-// 	wall_x -= floor(wall_x);
+static int	get_texture_dir(t_ray *ray);
 
-// 	int tex_x = (int)(wall_x * (double)data->tex_size);
-// 	if (side == 0 && ray_dir_x > 0)
-// 		tex_x = data->tex_size - tex_x - 1;
-// 	if (side == 1 && ray_dir_y < 0)
-// 		tex_x = data->tex_size - tex_x - 1;
-// }
+// step: how much to increase texture coordinates per pixel
+void	texture_func(t_gdata *data, t_ray ray, int x, int draw_start, int draw_end)
+{
+	int		y;
+	double	tex_pos;
+	double	step;
+	int		color;
+	int		dir = get_texture_dir(&ray);
+	int		tex_x;
+	int		tex_y;
+
+	double wall_x;
+	if (ray.side_hit == RAY_HIT_N_OR_S)
+		wall_x = data->player.pos.y + ray.perp_dist * ray.dir.y;
+	else
+		wall_x = data->player.pos.x + ray.perp_dist * ray.dir.x;
+	wall_x -= floor(wall_x);
+
+	tex_x = (int)(wall_x * data->tex_size);
+	if ((ray.side_hit == 0 && ray.dir.x > 0) || (ray.side_hit == 1 && ray.dir.y < 0))
+		tex_x = data->tex_size - tex_x - 1;
+
+	y = draw_start;
+	step = (1.0 * data->tex_size / ray.line_height);
+	tex_pos = (draw_start - W_HEIGHT / 2.0 + ray.line_height / 2.0) * step;
+	while (y < draw_end)
+	{
+		tex_y = (int)tex_pos & (data->tex_size - 1);
+		tex_pos += step;
+		color = data->textures[dir][data->tex_size * tex_y + tex_x];
+		put_pixel(&data->canvas, x, y++, color);
+	}
+}
 
 static int	get_texture_dir(t_ray *ray)
 {
@@ -47,39 +63,5 @@ static int	get_texture_dir(t_ray *ray)
 			return NORTH;
 		else
 			return SOUTH;
-	}
-}
-
-// step: how much to increase texture coordinates per pixel
-void	texture_func(t_gdata *data, t_ray ray, int x, int draw_start, int draw_end)
-{
-	int		y;
-	double	tex_pos;
-	double	step;
-	int		color;
-	int		dir = get_texture_dir(&ray);
-	int		tex_x;
-	int		tex_y;
-
-	double wall_x;
-	if (dir == RAY_HIT_E_OR_W)
-		wall_x = data->player.pos.y + ray.perp_dist * ray.dir.y;
-	else
-		wall_x = data->player.pos.x + ray.perp_dist * ray.dir.x;
-	wall_x -= floor(wall_x);
-
-	tex_x = (int)(wall_x * data->tex_size);
-	if ((ray.side_hit == 0 && ray.dir.x < 0) || (ray.side_hit == 1 && ray.dir.y > 0))
-		tex_x = data->tex_size - tex_x - 1;
-
-	y = draw_start;
-	step = (1.0 * data->tex_size / ray.line_height);
-	tex_pos = (draw_start - (double)W_HEIGHT / 2 + (double)ray.line_height / 2) * step;
-	while (y < draw_end)
-	{
-		tex_y = (int)tex_pos & (data->tex_size - 1);
-		tex_pos += step;
-		color = data->textures[dir][data->tex_size * tex_y + tex_x];
-		put_pixel(&data->canvas, x, y++, color);
 	}
 }
