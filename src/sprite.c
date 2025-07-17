6bbcb8e213d60ab26c529bc3ab253be451800705
abhi-v97 <v.abhishek@outlook.com>
2025-07-17 23:08:50 +0100

    norminette pass

--- src/sprite.c
+++ src/sprite.c
@@ -27,62 +27,71 @@ typedef struct s_sprite
 	int		draw_end_y;
 }	t_sprite;
 
-
 static void	calc_sprite_info(t_gdata *gd, t_sprite *sp);
 static void	render(t_gdata *gd, t_sprite *sp);
 
 void	draw_sprite(t_gdata *gd)
 {
-	t_sprite sp[1];
+	t_sprite	sp[1];
+	int			i;
 
 	sp[0].x = 2.5;
 	sp[0].y = 3.5;
-	for (int i = 0; i < 1; i++)
+	i = 0;
+	while (i < 1)
 	{
 		calc_sprite_info(gd, &sp[i]);
-		render(gd, &sp[i]);
+		render(gd, &sp[i++]);
+	}
+}
+
+static void	put_sprite(t_gdata *gd, t_sprite *sp, int x, int tex_x)
+{
+	int		y;
+	int		tex_y;
+	int		colour;
+
+	y = sp->draw_start_y;
+	while (y < sp->draw_end_y)
+	{
+		tex_y = ((((y << 8) - (W_HEIGHT << 7) + (sp->sprite_size << 7))
+					* gd->tex_size) / sp->sprite_size) >> 8;
+		colour = gd->textures[SPRITE][gd->tex_size * tex_y + tex_x];
+		if ((colour & 0x00FFFFFF) != 0)
+			put_pixel(&gd->canvas, x, y, colour);
+		y++;
 	}
 }
 
 static void	render(t_gdata *gd, t_sprite *sp)
 {
 	int		x;
-	int		y;
 	int		tex_x;
 
 	x = sp->draw_start_x;
-	y = sp->draw_start_y;
 	while (x < sp->draw_end_x)
 	{
-		tex_x = (int)((256 * (x - (-sp->sprite_size / 2 + sp->sprite_screen_x))
-					* gd->tex_size / sp->sprite_size) / 256);
-		if (sp->transform_y > 0 && x > 0 && x < W_WIDTH && sp->transform_y < gd->z_buffer[x])
-		{
-			while (y < sp->draw_end_y)
-			{
-				int d = (y << 8) - (W_HEIGHT << 7) + (sp->sprite_size << 7);
-				int texY = ((d * gd->tex_size) / sp->sprite_size) >> 8;
-				int colour = gd->textures[SPRITE][gd->tex_size * texY + tex_x];
-				if ((colour & 0x00FFFFFF) != 0)
-					put_pixel(&gd->canvas, x, y, colour);
-				y++;
-			}
-		}
+		tex_x = (((x + (sp->sprite_size >> 1) - sp->sprite_screen_x) << 8)
+				* gd->tex_size / sp->sprite_size) >> 8;
+		if (sp->transform_y > 0 && x > 0 && x < W_WIDTH && sp->transform_y
+			< gd->z_buffer[x])
+			put_sprite(gd, sp, x, tex_x);
 		x++;
 	}
 }
 
 static void	calc_sprite_info(t_gdata *gd, t_sprite *sp)
 {
-	sp->inv_det = 1.0 / (gd->plane.x * gd->dir.y - gd->dir.x *
-			gd->plane.y);
-	sp->transform_x = sp->inv_det * (gd->dir.y * (sp->x -
-				gd->player.pos.x) - gd->dir.x * (sp->y -
-					gd->player.pos.y));
-	sp->transform_y = sp->inv_det * (-gd->plane.y * (sp->x -
-				gd->player.pos.x) + gd->plane.x * (sp->y -
-					gd->player.pos.y));
-	sp->sprite_screen_x = (int)((W_WIDTH / 2.0) * (1 + sp->transform_x / sp->transform_y));
+	sp->inv_det = 1.0 / (gd->plane.x * gd->dir.y - gd->dir.x
+			* gd->plane.y);
+	sp->transform_x = sp->inv_det * (gd->dir.y * (sp->x
+				- gd->player.pos.x) - gd->dir.x * (sp->y
+				- gd->player.pos.y));
+	sp->transform_y = sp->inv_det * (-gd->plane.y * (sp->x
+				- gd->player.pos.x) + gd->plane.x * (sp->y
+				- gd->player.pos.y));
+	sp->sprite_screen_x = (int)((W_WIDTH / 2.0) * (1 + sp->transform_x
+				/ sp->transform_y));
 	sp->sprite_size = abs((int)(W_HEIGHT / (sp->transform_y)));
 	sp->draw_start_y = -sp->sprite_size / 2 + W_HEIGHT / 2;
 	if (sp->draw_start_y < 0)
